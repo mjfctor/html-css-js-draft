d9664e658290dc9d6c98cdaedfa2b75989a53f5c
Emjay Factor <emjayfactor@gmail.com>
2023-03-01 21:10:26 +0800

    Drafts

---
+++ draft.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+    document.getElementById("sample").innerHTML = "Paragraph changed.";
+}
+
+function sample() {
+    window.alert("VIRUS DETECTED");
+}
